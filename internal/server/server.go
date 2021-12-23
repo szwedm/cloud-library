ef5aa5f493749532deaf9f18ac68d8bc2960feb7
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2021-12-23 20:57:26 +0100

    Register userHandler in router

--- internal/server/server.go
+++ internal/server/server.go
@@ -32,7 +32,16 @@ func (s *server) registerBookPaths() {
 	s.router.HandleFunc("/books/{id:"+UUIDRegex+"}", s.booksHandler.deleteBookByID).Methods("DELETE")
 }
 
+func (s *server) registerUserPaths() {
+	s.router.HandleFunc("/users", s.usersHandler.getUsers).Methods("GET")
+	s.router.HandleFunc("/users", s.usersHandler.createUser).Methods("POST")
+	s.router.HandleFunc("/users/{id:"+UUIDRegex+"}", s.usersHandler.getUserByID).Methods("GET")
+	s.router.HandleFunc("/users/{id:"+UUIDRegex+"}", s.usersHandler.updateUser).Methods("PUT")
+	s.router.HandleFunc("/users/{id:"+UUIDRegex+"}", s.usersHandler.deleteUserByID).Methods("DELETE")
+}
+
 func (s *server) Run() {
 	s.registerBookPaths()
+	s.registerUserPaths()
 	log.Fatal(http.ListenAndServe(":8080", s.router))
 }
